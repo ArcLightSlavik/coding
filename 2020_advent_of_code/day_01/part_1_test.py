4a813ff9bfdb6a479536d3750fa7f8e87b284fb9
t-leskv <viacheslav.leskiv@eleks.com>
2021-05-04 19:02:00 +0300

    Test failure

--- 2020_advent_of_code/day_01/part_1_test.py
+++ 2020_advent_of_code/day_01/part_1_test.py
@@ -14,7 +14,7 @@ def test_simple(input_s: str, expected: int) -> None:
 
 @pytest.mark.parametrize(
     ("input_s", "expected"),
-    (("1721 979 366 299 675 1456", 514579),),
+    (("1721 979 366 299 675 1456", 5145788),),
 )
 def test(input_s: str, expected: int) -> None:
     assert execute(input_s) == expected
