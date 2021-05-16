823afa67fe699a13ba90878e127e5082ccb962d7
ArcLight_Slavik <arclight.leskiv@gmail.com>
2021-05-16 08:05:01 +0300

    File loading (#2)
    * Make file handling slightly better

--- 2020_advent_of_code/day_01/part_2_test.py
+++ 2020_advent_of_code/day_01/part_2_test.py
@@ -1,11 +1,21 @@
 import pytest
 
 from .part_2 import execute
+from .part_2 import main
 
 
 @pytest.mark.parametrize(
     ("input_s", "expected"),
     (("1721 979 366 299 675 1456", 241861950),),
 )
-def test(input_s: str, expected: int) -> None:
+def test_execute(input_s: str, expected: int) -> None:
     assert execute(input_s) == expected
+
+
+def test_execute_exception() -> None:
+    with pytest.raises(NotImplementedError):
+        execute("")
+
+
+def test_main() -> None:
+    assert main() == 165795564
