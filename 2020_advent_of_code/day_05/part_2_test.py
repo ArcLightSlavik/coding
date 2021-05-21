d7694b3f0d86b62b38a3307ebcd905652468570e
ArcLightSlavik <arclight.leskiv@gmail.com>
2021-05-21 18:09:50 +0300

    day_04

---
+++ 2020_advent_of_code/day_05/part_2_test.py
@@ -0,0 +1,42 @@
+import pytest
+
+from .part_2 import execute
+from .part_2 import execute_simple
+from .part_2 import main
+from .part_2 import main_simple
+
+INPUT_S = """\
+BBBFFFFRLL
+BBBFFFFRLR
+BBBFFFFRRL
+BBBFFFFRRR
+BBBFFFBLLL
+BBBFFFBLLR
+BBBFFFBLRR
+BBBFFFBRLL
+BBBFFFBRLR
+"""
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, 906),),
+)
+def test_execute_simple(input_s: str, expected: int) -> None:
+    assert execute_simple(input_s) == expected
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, 906),),
+)
+def test_execute(input_s: str, expected: int) -> None:
+    assert execute(input_s) == expected
+
+
+def test_main_simple() -> None:
+    assert main_simple() == 554
+
+
+def test_main() -> None:
+    assert main() == 554
