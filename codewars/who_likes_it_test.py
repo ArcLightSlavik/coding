1829f93ce3b547426cd74ef0e96275bb13ecf5c0
ArcLightSlavik <arclight.leskiv@gmail.com>
2021-06-07 00:01:08 +0300

    Add code wars

---
+++ codewars/who_likes_it_test.py
@@ -0,0 +1,17 @@
+import pytest
+
+from .who_likes_it import execute
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    (
+        ([], "no one likes this"),
+        (["Peter"], "Peter likes this"),
+        (["Jacob", "Alex"], "Jacob and Alex like this"),
+        (["Max", "John", "Mark"], "Max, John and Mark like this"),
+        (["Alex", "Jacob", "Mark", "Max"], "Alex, Jacob and 2 others like this"),
+    ),
+)
+def test_basic(input_s: list[str], expected: str) -> None:
+    assert execute(input_s) == expected
